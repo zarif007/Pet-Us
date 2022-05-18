18a1ca174cd2b93ca1708a6cde7d8425a7e5408a
zarif007 <zarifhuq786@gmail.com>
2022-05-18 18:37:00 +0600

    Adding funcs for wallet

---
+++ test/TestAdoption.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.16;
+
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+  // The address of the adoption contract to be tested
+  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+  // The id of the pet that will be used for testing
+  uint expectedPetId = 8;
+
+  // The expected owner of adopted pet is this contract
+  address expectedAdopter = address(this);
+
+}
